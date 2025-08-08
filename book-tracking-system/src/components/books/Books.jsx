3c8f83c3177c32a7461fcc4620872180b6c0dafe
Megha <meghaninave8@gmail.com>
2025-08-08 21:12:40 +0530

    ....

--- book-tracking-system/src/components/books/Books.jsx
+++ book-tracking-system/src/components/books/Books.jsx
@@ -2,11 +2,12 @@ import {useState} from 'react'
 
 export default function 
 () {
-    const [booklist,setBooklist]=useState([{id:1,title:"abc",author:xyz}
+    const [booklist,setBooklist]=useState([{title:"abc",author:"xyz"}
 
     ]);
     const [title,setTitle]=useState("");
     const [author,setAuthor]=useState("");
+    const [index,setIndex]=useState(0);
     const Addbook=()=>{
         const newbook={
             title:title,
@@ -17,6 +18,18 @@ export default function
         setBooklist([...booklist,newbook])
     }
 
+   function deleteb(index) {
+    let temp = [...booklist];  
+    temp.splice(index, 1);     
+    setBooklist(temp);         
+  }
+  function updateb(index)
+  {
+    setIndex(index);
+    setTitle(booklist[index].title);
+    setAuthor(booklist[index].author);
+  }
+
 
   return (
     <div>
@@ -24,10 +37,13 @@ export default function
         <input type="text" placeholder='enter book AUTHOR' value={author} onChange={(e)=>setAuthor(e.target.value)} />
         <button onClick={Addbook}>Addbook</button>
         {
-            booklist.map((book,i)=>{
-                <p>{book.title}-{book.author}</p>
+            booklist.map((book,i)=>(
+                <p>{book.title}-{book.author}
+                <button className='de' onClick={()=>deleteb(i)}>Delete</button>
+                <button className='up' onClick={()=>updateb(i)}>Update</button>
+                </p>
 
-            })
+            ))
         }
         
 
