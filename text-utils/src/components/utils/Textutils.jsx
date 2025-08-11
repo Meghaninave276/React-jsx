3064deaff2031df6304474cb167663e520af214e
Megha <meghaninave8@gmail.com>
2025-08-11 21:53:37 +0530

    11-august-2025

--- text-utils/src/components/utils/Textutils.jsx
+++ text-utils/src/components/utils/Textutils.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import {use, useState} from 'react';
+import "./Textutils.css";
 
 export default function Textutils() {
+    const [text,setText]=useState("");
+    const [char,setChar]=useState(0);
+    const [words,setWords]=useState(0);
+    const [upper,setupper]=useState("");
+    const [lower,setlower]=useState("");
+    const [read,setRead]=useState(0);
+    const [vowels,setVowels]=useState(0);
+
+    
+
+
+
+    const handletext=(e)=>setText(e.target.value);
+    const handlechar=()=>{
+        setChar(text.length);
+
+    }
+    const handlewords=()=>{
+        const word=text.split(" ");
+        setWords(word.length);
+    }
+    const handleupper=()=>{
+        setupper(text.toUpperCase());
+
+    }
+    const handlelower=()=>{
+        setlower(text.toLowerCase());
+
+    }
+    const handlereadtime=()=>{
+        const word=text.split(" ");
+        setRead(word.length*0.25+"minutes");
+    }
+    const handlevowels=()=>{
+        const vowelCount = text.toLowerCase().match(/[aeiou]/g)?.length || 0;
+    setVowels(vowelCount);
+    }
+   
+
   return (
-    <div>Textutils</div>
+    <div className='textutils-container'>
+        <div >
+            <input type="text"  placeholder='enter text' value={text} onChange={handletext}/>
+
+        </div>
+        
+        <div className='p'>
+            <p>{text}</p>
+
+        </div>
+     
+        <div className='buttons'>
+            <button className='char' onClick={handlechar}>char</button>
+            <button className='word' onClick={handlewords}>words</button>
+            <button className='upper' onClick={handleupper}>upper</button>
+            <button className='lower' onClick={handlelower}>lower</button>
+            <button className='time' onClick={handlereadtime}>time</button>
+            <button className='vowels' onClick={handlevowels}>vowels</button>
+        </div>
+       
+        <div className='w'>
+            <h3>total char length:{char}</h3>
+            <h3>total words length:{words}</h3>
+            <h3>uppercase:{upper}</h3>
+            <h3>lowercase:{lower}</h3>
+            <h3>Time to Read:{read}</h3>
+            <h3>total vowels:{vowels}</h3>
+
+        </div>
+    </div>
   )
 }
