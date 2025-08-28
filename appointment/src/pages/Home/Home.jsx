5d3152fc06b29171b4b09157321347af74804ba6
Megha <meghaninave8@gmail.com>
2025-08-28 21:27:19 +0530

    ...

--- appointment/src/pages/Home/Home.jsx
+++ appointment/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Contact from '../../components/Contact/Contact'
+import Displayd from '../../components/Displayd/Displayd'
 
 export default function Home() {
   return (
     <div>
         <Contact/>
+        <Displayd />
     </div>
   )
 }
