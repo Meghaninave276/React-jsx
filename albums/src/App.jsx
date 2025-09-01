5846ac248cafe9813fb78674ab68b0c58656205e
Megha <meghaninave8@gmail.com>
2025-09-01 20:34:15 +0530

    1-sep-2025

--- albums/src/App.jsx
+++ albums/src/App.jsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Album from './components/Album/Album'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
 
   return (
     <>
+    <Navbar/>
+    <Hero/>
+    <Album/>
+    <Footer/>
      
     </>
   )
---
+++ albums/src/components/Album/Album.jsx
@@ -0,0 +1,265 @@
+import React from 'react'
+import "./Album.css";
+export default function Album() {
+  return (
+   <div className="album py-5 bg-light">
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div className="col">
+            <div className="card shadow-sm">
+             <img src="http://hdqwalls.com/wallpapers/beautiful-sea-coast-hd.jpg" alt="" />
+              <div className="card-body">
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="btn-group">
+                    <button
+                      type="button"
+                      className="btn btn-sm btn-outline-secondary"
+                    >
+                      View
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-sm btn-outline-secondary"
+                    >
+                      Edit
+                    </button>
+                  </div>
+                  <small className="text-muted">9 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+      <div className="card shadow-sm">
+        <img src="https://cdn.wallpapersafari.com/74/5/zwaDvx.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col">
+      <div className="card shadow-sm">
+      <img src="https://www.hdwallpapers.in/download/beautiful_sea_view_horizon_clouds_landscape_ship_blue_sky_water_hd_nature-2560x1440.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col">
+      <div className="card shadow-sm">
+       <img src="https://velstana.com/wp-content/uploads/home-black-sea-coast.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col">
+      <div className="card shadow-sm">
+       <img src="https://wallpaperaccess.com/full/1321412.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col">
+      <div className="card shadow-sm">
+      <img src="https://wallpaperbat.com/img/505826-high-resolution-stormy-ocean-1920x1080-download-hd-wallpaper-wallpapertip.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col">
+      <div className="card shadow-sm">
+      <img src="https://wallpaperaccess.com/full/713343.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col">
+      <div className="card shadow-sm">
+       <img src="https://welcomesaudi-media.s3.me-south-1.amazonaws.com/0000/64/2025/08/27/4-saudi-arabias-aroya-cruises-expands-operations-to-mediterranean-after-red-sea.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+             <div className="col">
+      <div className="card shadow-sm">
+        <img src="https://media.inmobalia.com/imgV1/B8vEv5Xh_VThvnEqMx4G0ZxU~~H0Ar_8Bn_AfAx~4dnt796zQ9QIPwzdr9PkzIdRpbeOYktBAPFfTt23HPhndt5b7cRr2vg76pwDjdUVJPRRNIpp1tuktyOrdmdg9u~ajchUyAklOlHvudBgHMOl3~5Mwg~T8~_5CZDyB0tyaCeVpPwzQ7cGG6PdJsYBp9V7~nemm5xs5cGzTk6V1cYZthaSG8A3GeMKY~iIwJ_qdYFVXGjZTBtK5li8SetGhTA1c1pQRkUpfAEvQ1y4Ei2jFDwEckVjt~uwhdT~QVn434j34UmdduO8fObaYLZ0L6IEjXNyicBVYl5A980L6n2HAZtVH7eYi5YvUAu0xX7A5ojSmk3XrIYZTF6Vr~vSzmXhWg--.jpg" alt="" />
+        <div className="card-body">
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <div className="d-flex justify-content-between align-items-center">
+            <div className="btn-group">
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                View
+              </button>
+              <button
+                type="button"
+                className="btn btn-sm btn-outline-secondary"
+              >
+                Edit
+              </button>
+            </div>
+            <small className="text-muted">9 mins</small>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
