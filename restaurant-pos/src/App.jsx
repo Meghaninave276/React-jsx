fbdb0c5f186bb34a32be996f7e6104f98590dabc
Megha <meghaninave8@gmail.com>
2025-10-12 21:22:06 +0530

    12/10/25

--- restaurant-pos/src/App.jsx
+++ restaurant-pos/src/App.jsx
@@ -2,18 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './Components/Home/Home'
-import LoginPage from './Components/Login/Login'
-import Login from './Components/Login/Login'
-import Dashboard from './Components/Dashboard/Dashboard'
+import Screen from './Screen/Screen'
+import { BrowserRouter } from 'react-router-dom'
+
 
 function App() {
   
 
   return (
     <>
- <Login/>
- <Home/>
+<BrowserRouter>
+
+<Screen/>
+</BrowserRouter>
 
  
 
