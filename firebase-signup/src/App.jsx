3349b15344b2550f529bb883c9700fc5b26591ef
Megha <meghaninave8@gmail.com>
2025-11-11 13:01:23 +0530

    11/11/25

--- firebase-signup/src/App.jsx
+++ firebase-signup/src/App.jsx
@@ -2,7 +2,7 @@
 import { useState } from 'react'
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword,signInWithPopup } from 'firebase/auth';
 import { auth,provider,db } from './firebase';
-import { collection, addDoc,getDocs } from "firebase/firestore"; 
+import { collection, addDoc,getDocs,doc,deleteDoc ,setDoc,updateDoc, deleteField} from "firebase/firestore"; 
 import './App.css'
 
 function App() {
@@ -48,15 +48,34 @@ const handleadddata=()=>{
     querySnapshot.docs.forEach((doc) => console.log(doc.data()));
   
   }
+  const handlesetdata=()=>{
+    setDoc(doc(db, "user","12345"),{
+      name:"megha",
+      age:19,
+      city:"surat"
+    }).then(()=>alert("data set succesfully")).catch((error)=>alert(error.message));
+  }
+  const handledeletedoc=async()=>{
+    const userref=doc(db,"user","EjUhSCtY16DsnnXeoGyl");
+    await updateDoc(userref,{
+      name:deleteField()
+    }).then(()=>alert("data deleted sucessfully!!" )).catch((error)=>alert(error.message));
+  }
   return (
     <>
-      <input type="text" placeholder='email' onChange={(e) => setEmail(e.target.value)} />
-      <input type="text" placeholder='password' onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleSignUp}>Sign Up</button>
-      <button onClick={handleSignIn}>Sign In</button>
-      <button onClick={handlesigninwithgoogle}>Sign In with google</button>
-      <button onClick={handleadddata}>data add</button>
-      <button onClick={handlereaddata}>read data </button>
+     <div className="App">
+    <h1>Firebase Auth Demo</h1>
+    <input type="text" placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
+    <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} />
+    <button onClick={handleSignUp}>Sign Up</button>
+    <button onClick={handleSignIn}>Sign In</button>
+    <button onClick={handlesigninwithgoogle}>Sign In with Google</button>
+    <button onClick={handleadddata}>Add Data</button>
+    <button onClick={handlereaddata}>Read Data</button>
+    <button onClick={handlesetdata}>set Data</button>
+    <button onClick={handledeletedoc}>delete Data</button>
+    <footer>✨ Powered by Firebase</footer>
+  </div>
 
     </>
   )
